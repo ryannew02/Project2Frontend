1089ff8a9d3754ab4a6ea96a4fda706aff5625e3
Ryan Juergens <ryanjuergens@mail.com>
2026-07-05 15:54:48 -0400

    Major rehaul and integration of new datastructures

--- Test/Backend/src/Location.h
+++ Test/Backend/src/Location.h
@@ -14,5 +14,18 @@ struct Location {
     string city;
 	Location() {};
 	Location(string address, double longitude, double latitude) : fullAddress(address), longitude(longitude), latitude(latitude) {};
-	
+	Location& operator=(const Location& other) 
+	{
+		if (this != &other) {
+			longitude = other.longitude;
+			latitude = other.latitude;
+			fullAddress = other.fullAddress;
+			addressNumber = other.addressNumber;
+			addressName = other.addressName;
+			zip = other.zip;
+			city = other.city;
+		}
+		return *this;
+	}	
 };
+
--- Test/Backend/src/main.cpp
+++ Test/Backend/src/main.cpp
@@ -4,131 +4,186 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <map> //Will need to remove this for final submission when you implement loading of CSV
 #include "SplayTree.h"
 #include "Location.h"
 #include "Trie.h"
 
-using namespace std;
+constexpr double PI = 3.14159265358979323846;
 
-void print(SplayTree<Location, string> tree);
+using namespace std;
+SplayTree<Location, string> loadSplayCSV(const string& filepath, const string& zipFilepath);
+Trie<Location> loadTrieCSV(const string& filepath, const string& zipFilepath);
+string getStringValue(const string& json, const string& key);
+vector<string> getStringArray(const string& json, const string& key);
+double haversine(double lat1, double lon1, double lat2, double lon2);
+void runIO(SplayTree<Location, string>& splayTree, Trie<Location>& trieTree);
 
 int main() {
+	string addressFilepath = "Test/Backend/data/addresses.csv";
+	string zipCityFilepath = "Test/Backend/data/zip_city.csv";
 	SplayTree<Location, string> splayTree;
-
-	Location loc1("1313 N Collier Blvd", 5.8f, 5.2f);
-	Location loc2("4365 Kauila St", 2.4f, 8.1f);
-	Location loc3("2090 Hanalima St", 1.8f, 9.4f);
-
-	splayTree.insert(&loc1, loc1.fullAddress);
-	print(splayTree);
-	splayTree.insert(&loc2, loc2.fullAddress);
-	print(splayTree);
-	splayTree.insert(&loc3, loc3.fullAddress);
-	print(splayTree);
-
+	Trie<Location> trieTree;
+	splayTree = loadSplayCSV(addressFilepath, zipCityFilepath);
+	trieTree = loadTrieCSV(addressFilepath, zipCityFilepath);
+	runIO(splayTree, trieTree);
 	return 0;
 }
 
-void print(SplayTree<Location, string> tree) {
-	auto dataPtrs = tree.getPreOrder();
-	for (unsigned int i = 0; i < dataPtrs.size(); i++)
-		cout << dataPtrs[i]->fullAddress << endl;
-	cout << endl << endl;
+//csv parser...
+
+SplayTree<Location, string> loadSplayCSV(const string& filepath,
+const string& zipFilepath) {
+	SplayTree<Location, string> splayTree;
+	map<string, string> zipCityMap;
+	ifstream file(filepath);
+	ifstream zipFile(zipFilepath);
+	string line;
+	string zipLine;
+	
+	// Skip header
+	getline(zipFile, zipLine);
+
+	while (getline(zipFile, zipLine)) {
+		if (zipLine.empty()) continue;
+		// Remove carriage return if present
+		if (!zipLine.empty() && zipLine.back() == '\r') zipLine.pop_back();
+
+		// Split CSV columns: zip, city
+		stringstream ss(zipLine);
+		string zipLookup, cityLookup;
+		//example format 33801, Lakeland
+		getline(ss, zipLookup, ',');
+		getline(ss, cityLookup);
+		zipCityMap[zipLookup] = cityLookup;
+	}
+
+	// Skip header
+	getline(file, line);
+
+	while (getline(file, line)) {
+		if (line.empty()) continue;
+
+		// Remove carriage return if present
+		if (!line.empty() && line.back() == '\r') line.pop_back();
+
+		// Split CSV columns: lat, lon, addressNumber, addressName, unit, zip
+		stringstream ss(line);
+		string latStr, lonStr, addressNumber, addressName, unit, zip;
+		//example format 28.56656102,-81.36566349,1650,N Mills Ave,Apartment 313,32828
+		getline(ss, latStr, ',');
+		getline(ss, lonStr, ',');
+		getline(ss, addressNumber, ',');
+		getline(ss, addressName, ',');
+		getline(ss, unit, ',');
+		getline(ss, zip);
+
+		// Store CSV row values in an Address object
+		Location* a = new Location();
+		a->latitude = stod(latStr);
+		a->longitude = stod(lonStr);
+		a->addressNumber = addressNumber;
+		a->addressName = addressName;
+		a->zip = zip;
+
+		if (zipCityMap.count(zip)) {
+			a->city = zipCityMap.at(zip);
+		}
+
+		if (!unit.empty()) {
+
+			a->fullAddress = addressNumber + " " + addressName + " " + unit;
+
+		} else {
+
+			a->fullAddress = addressNumber + " " + addressName;
+
+		}
+
+		// Store address using the display address as the lookup key
+		string key = a->fullAddress;
+		splayTree.insert(a , key);
+	}
+
+	return splayTree;
 }
 
-//_____________________CSV Parser needs to be updated to accomadate new object_______________
-//_____________________CSV Parser needs to be updated to accomadate new object_______________
-//_____________________CSV Parser needs to be updated to accomadate new object_______________
+Trie<Location> loadTrieCSV(const string& filepath,
+const string& zipFilepath) {
+	Trie<Location> trieTree;
+	map<string, string> zipCityMap;
+	ifstream file(filepath);
+	ifstream zipFile(zipFilepath);
+	string line;
+	string zipLine;
+	
+	// Skip header
+	getline(zipFile, zipLine);
 
-			// // ─── Start of updated CSV Parser ────────────────────────────────────────────────────────────
-			// std::map<std::string, std::string> loadZipCity(const std::string& filepath) {
-			//     std::map<std::string, std::string> zipCityMap;
-			//     std::ifstream file(filepath);
-			//     std::string line;
+	while (getline(zipFile, zipLine)) {
+		if (zipLine.empty()) continue;
+		// Remove carriage return if present
+		if (!zipLine.empty() && zipLine.back() == '\r') zipLine.pop_back();
 
-			//     // Skip header
-			//     std::getline(file, line);
+		// Split CSV columns: zip, city
+		stringstream ss(zipLine);
+		string zipLookup, cityLookup;
+		//example format 33801, Lakeland
+		getline(ss, zipLookup, ',');
+		getline(ss, cityLookup);
+		zipCityMap[zipLookup] = cityLookup;
+	}
 
-			//     while (std::getline(file, line)) {
-			//         if (line.empty()) continue;
+	// Skip header
+	getline(file, line);
 
-			//         if (!line.empty() && line.back() == '\r') line.pop_back();
+	while (getline(file, line)) {
+		if (line.empty()) continue;
 
-			//         std::stringstream ss(line);
-			//         std::string zip;
-			//         std::string city;
+		// Remove carriage return if present
+		if (!line.empty() && line.back() == '\r') line.pop_back();
 
-			//         std::getline(ss, zip, ',');
-			//         std::getline(ss, city, ',');
+		// Split CSV columns: lat, lon, addressNumber, addressName, unit, zip
+		stringstream ss(line);
+		string latStr, lonStr, addressNumber, addressName, unit, zip;
+		//example format 28.56656102,-81.36566349,1650,N Mills Ave,Apartment 313,32828
+		getline(ss, latStr, ',');
+		getline(ss, lonStr, ',');
+		getline(ss, addressNumber, ',');
+		getline(ss, addressName, ',');
+		getline(ss, unit, ',');
+		getline(ss, zip);
 
-			//         zipCityMap[zip] = city;
-			//     }
+		// Store CSV row values in an Address object
+		Location* a = new Location();
+		a->latitude = stod(latStr);
+		a->longitude = stod(lonStr);
+		a->addressNumber = addressNumber;
+		a->addressName = addressName;
+		a->zip = zip;
 
-			//     return zipCityMap;
-			// }
+		if (zipCityMap.count(zip)) {
+			a->city = zipCityMap.at(zip);
+		}
 
-			// // ─── Original CSV Parser ────────────────────────────────────────────────────────────
-			// std::map<std::string, Address> loadCSV(const std::string& filepath,
-			//                                        const std::map<std::string, std::string>& zipCityMap) {
-			//     std::map<std::string, Address> addressMap;
-			//     std::ifstream file(filepath);
-			//     std::string line;
+		if (!unit.empty()) {
 
-			//     // Skip header
-			//     std::getline(file, line);
+			a->fullAddress = addressNumber + " " + addressName + " " + unit;
 
-			//     while (std::getline(file, line)) {
-			//         if (line.empty()) continue;
+		} else {
 
-			//         // Remove carriage return if present
-			//         if (!line.empty() && line.back() == '\r') line.pop_back();
+			a->fullAddress = addressNumber + " " + addressName;
 
-			//         // Split CSV columns: lat, lon, addressNumber, addressName, unit, zip
-			//         std::stringstream ss(line);
-			//         std::string latStr, lonStr, addressNumber, addressName, unit, zip;
+		}
 
-			//         std::getline(ss, latStr, ',');
-			//         std::getline(ss, lonStr, ',');
-			//         std::getline(ss, addressNumber, ',');
-			//         std::getline(ss, addressName, ',');
-			//         std::getline(ss, unit, ',');
-			//         std::getline(ss, zip, ',');
+		// Store address using the display address as the lookup key
+		string key = a->fullAddress;
+		trieTree.insert(a , key);
+	}
 
-			//         // Store CSV row values in an Address object
-			//         Address a;
-			//         a.lat = std::stod(latStr);
-			//         a.lon = std::stod(lonStr);
-			//         a.addressNumber = addressNumber;
-			//         a.addressName = addressName;
-			//         a.zip = zip;
-
-			//         if (zipCityMap.count(zip)) {
-			//             a.city = zipCityMap.at(zip);
-			//         }
-
-			//         if (!unit.empty()) {
-
-			//             a.fullAddress = addressNumber + " " + addressName + " " + unit;
-
-			//         } else {
-
-			//             a.fullAddress = addressNumber + " " + addressName;
-
-			//         }
-
-			//         // Store address using the display address as the lookup key
-			//         std::string key = a.fullAddress;
-			//         addressMap[key] = a;
-			//     }
-
-			//     return addressMap;
-			// }
-
-//_____________________CSV Parser needs to be updated to accomadate new object_______________
-//_____________________CSV Parser needs to be updated to accomadate new object_______________
-//_____________________CSV Parser needs to be updated to accomadate new object_______________
-
-
+	return trieTree;
+}
 
 // _______________________________ I/O handling ______________________________
 
@@ -172,10 +227,10 @@ vector<string> getStringArray(const string& json, const string& key) {
 
 double haversine(double lat1, double lon1, double lat2, double lon2) {
     const double EarthR = 3958.8;
-    double dLat = (lat2 - lat1) * M_PI / 180.0;
-    double dLon = (lon2 - lon1) * M_PI / 180.0;
+    double dLat = (lat2 - lat1) * PI / 180.0;
+    double dLon = (lon2 - lon1) * PI / 180.0;
     double a = sin(dLat/2) * sin(dLat/2) +
-               cos(lat1 * M_PI / 180.0) * cos(lat2 * M_PI / 180.0) *
+               cos(lat1 * PI / 180.0) * cos(lat2 * PI / 180.0) *
                sin(dLon/2) * sin(dLon/2);
     return EarthR * 2 * atan2(sqrt(a), sqrt(1-a));
 }
@@ -198,63 +253,61 @@ string input;
         bool hasPrev = false;
 
         for (unsigned int i = 0; i < requestedAddresses.size(); i++) {
-            const string& req = requestedAddresses[i];
-//need to pass an option on which tree to search like a boolean...
-//need to find a replacement for count function to verify address is in the listing or implement a try catch response in the tree search algo
-//need to identify how to pass the details into the tree search algo
+            string req = requestedAddresses[i];
             
 			if(algorithm == "splay")
 			{
-
-			if (addressMap.count(req)) {
-                Location& a = addressMap[req];
-
-                if (hasPrev) {
-                    totalDistance += haversine(prev.latitude, prev.longitude, a.latitude, a.longitude);
-                }
-                prev = a;
-                hasPrev = true;
-
-                foundJson += "{\"address\":\"" + a.fullAddress + "\","
-                        + "\"city\":\"" + a.city + "\","
-                        + "\"zip\":\"" + a.zip + "\","
-                        + "\"lat\":" + to_string(a.latitude) + ","
-                        + "\"lon\":" + to_string(a.longitude) + "}";
-            } else {
+				Location* a = splayTree.search(req); //search still needs to be implemented for this datastructure
+				if(a == nullptr){
                 cerr << "NOT FOUND: " << req << endl;
                 foundJson += "{\"address\":\"" + req + "\",\"lat\":0,\"lon\":0,\"error\":\"not found\"}";
-            }
+            	if (i < requestedAddresses.size() - 1) foundJson += ",";
+				continue;
+				}
+				else if (hasPrev) {
+					totalDistance += haversine(prev.latitude, prev.longitude, a->latitude, a->longitude);
+					}
+					prev = *a;
+					hasPrev = true;
 
-            if (i < requestedAddresses.size() - 1) foundJson += ",";
-				
+					foundJson += "{\"address\":\"" + a->fullAddress + "\","
+							+ "\"city\":\"" + a->city + "\","
+							+ "\"zip\":\"" + a->zip + "\","
+							+ "\"lat\":" + to_string(a->latitude) + ","
+							+ "\"lon\":" + to_string(a->longitude) + "}";
+
+				if (i < requestedAddresses.size() - 1) foundJson += ",";
 			}
 
-			if(algorithm == "trie")
+			else if(algorithm == "trie")
 			{
-
-
-			if (addressMap.count(req)) {
-                Location& a = addressMap[req];
-
-                if (hasPrev) {
-                    totalDistance += haversine(prev.latitude, prev.longitude, a.latitude, a.longitude);
-                }
-                prev = a;
-                hasPrev = true;
-
-                foundJson += "{\"address\":\"" + a.fullAddress + "\","
-                        + "\"city\":\"" + a.city + "\","
-                        + "\"zip\":\"" + a.zip + "\","
-                        + "\"lat\":" + to_string(a.latitude) + ","
-                        + "\"lon\":" + to_string(a.longitude) + "}";
-            } else {
+                Location* a = trieTree.search(req); //search still needs to be implemented for this datastructure
+				if(a == nullptr){
                 cerr << "NOT FOUND: " << req << endl;
                 foundJson += "{\"address\":\"" + req + "\",\"lat\":0,\"lon\":0,\"error\":\"not found\"}";
-            }
+            	if (i < requestedAddresses.size() - 1) foundJson += ",";
+				continue;
+            	}
+                else if (hasPrev) {
+                    totalDistance += haversine(prev.latitude, prev.longitude, a->latitude, a->longitude);
+                }
+                prev = *a;
+                hasPrev = true;
 
-            if (i < requestedAddresses.size() - 1) foundJson += ",";
+                foundJson += "{\"address\":\"" + a->fullAddress + "\","
+                        + "\"city\":\"" + a->city + "\","
+                        + "\"zip\":\"" + a->zip + "\","
+                        + "\"lat\":" + to_string(a->latitude) + ","
+                        + "\"lon\":" + to_string(a->longitude) + "}";
 
+            	if (i < requestedAddresses.size() - 1) foundJson += ",";
 			}
+			//Address doesn't exist in either tree
+			else {
+                cerr << "NOT FOUND: " << req << endl;
+                foundJson += "{\"address\":\"" + req + "\",\"lat\":0,\"lon\":0,\"error\":\"not found\"}";
+            	if (i < requestedAddresses.size() - 1) foundJson += ",";
+            }
 			
         }
         foundJson += "]";
