013e243a57ee4da0b9eae6079e93ff0168eaea37
Ryan Juergens <ryanjuergens@mail.com>
2026-07-05 10:56:38 -0400

    Changes the Location object to double from float

--- Test/Backend/src/Location.h
+++ Test/Backend/src/Location.h
@@ -5,11 +5,11 @@
 using namespace std;
 
 struct Location {
-	float longitude = 0;
-	float latitude = 0;
+	double longitude = 0;
+	double latitude = 0;
 	string address;
 
 	Location() {};
-	Location(string address, float longitude, float latitude) : address(address), longitude(longitude), latitude(latitude) {};
+	Location(string address, double longitude, double latitude) : address(address), longitude(longitude), latitude(latitude) {};
 	
 };
