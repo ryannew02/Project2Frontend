fd509f92eb9abb5b0937698c06f13ee6f0674ca2
Ryan Juergens <ryanjuergens@mail.com>
2026-07-07 21:26:25 -0400

    bugfixing fileopening issue

--- Test/Backend/src/main.cpp
+++ Test/Backend/src/main.cpp
@@ -23,8 +23,11 @@ double haversine(double lat1, double lon1, double lat2, double lon2);
 void runIO(SplayTree<Location, string>& splayTree, Trie<Location>& trieTree);
 
 int main() { 
-    string addressFilepath = "/data/addresses.csv";
-    string zipCityFilepath = "/data/zip_city.csv";
+    const char* dataDirEnv = getenv("VAW_DATA_DIR");
+    string dataDir = dataDirEnv ? string(dataDirEnv) : "data";
+
+    string addressFilepath = dataDir + "/addresses.csv";
+    string zipCityFilepath = dataDir + "/zip_city.csv";
 	SplayTree<Location, string> splayTree = loadSplayCSV(addressFilepath, zipCityFilepath);
 	Trie<Location> trieTree = loadTrieCSV(addressFilepath, zipCityFilepath);
 
